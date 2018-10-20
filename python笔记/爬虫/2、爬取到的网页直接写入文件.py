edb8fef9c0d8e6e702f3df9a676d0721c70eba8a
Yudanqu <943775910@qq.com>
2018-10-20 20:35:39 +0800

    del

--- python笔记/爬虫/2、爬取到的网页直接写入文件.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-
-urllib.request.urlretrieve("http://www.baidu.com",
-                           filename=r"F:/python_note/爬虫/file/file2.html")
-
-# urlretrieve在执行过程中，会产生一些缓存
-# 清除缓存
-urllib.request.urlcleanup()
